bac1183a036c1067d8106c3dd613d927c6e9c545
LoganKells <lkells@outlook.com>
2022-08-01 18:17:06 -0400

    Initial Commit

---
+++ recipe-list/recipe_listApp.swift
@@ -0,0 +1,17 @@
+//
+//  recipe_listApp.swift
+//  recipe-list
+//
+//  Created by Logan Kells on 8/1/22.
+//
+
+import SwiftUI
+
+@main
+struct recipe_listApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
